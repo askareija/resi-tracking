a88c53a13f1887209fc7be37dd9fa00362fb4077
Aden Aziz Hasnayakin <aden@kodelokus.com>
2019-05-19 15:14:13 +0700

    Avatar on topbar

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -4,9 +4,21 @@
     <v-toolbar-title>Resi Tracking</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn small flat>
-        <v-icon dark>more_vert</v-icon>
-      </v-btn>
+      <v-menu bottom left>
+        <template v-slot:activator="{ on }">
+          <v-btn fab dark icon v-on="on">
+            <v-avatar>
+              <v-img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"/>
+            </v-avatar>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title>asd</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-toolbar-items>
   </v-toolbar>
 </template>
