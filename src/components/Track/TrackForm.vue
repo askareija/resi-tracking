8722864a5f71561ee31791423a6ef93f0e851c8a
Aden Aziz Hasnayakin <adeniscute@gmail.com>
2019-06-01 21:04:18 +0700

    dialog template

--- src/components/Track/TrackForm.vue
+++ src/components/Track/TrackForm.vue
@@ -24,14 +24,45 @@
         </v-btn>
       </v-flex>
     </v-layout>
+    <v-btn
+      :disabled="dialog"
+      :loading="dialog"
+      class="white--text"
+      color="purple darken-2"
+      @click="dialog = true"
+      >Start loading</v-btn
+    >
+    <v-dialog v-model="dialog" hide-overlay persistent width="300">
+      <v-card color="primary" dark>
+        <v-card-text>
+          Please stand by
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-form>
 </template>
 
 <script>
 import ExpeditionTypeItems from './ExpeditionTypeItems'
 export default {
+  data() {
+    return {
+      dialog: false
+    }
+  },
   components: {
     ExpeditionTypeItems
+  },
+  watch: {
+    // dialog(val) {
+    //   if (!val) return
+    //   setTimeout(() => (this.dialog = false), 4000)
+    // }
   }
 }
 </script>
